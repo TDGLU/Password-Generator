dd467ebc90c078271208806710e5f1e5a4ff54ad
TDGLU <tdglukealt@gmail.com>
2022-10-02 00:50:51 -0700

    added to script, added all possible characters, added prompt when clicking generate password

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  console.log('Generate Password button clicked.')
+
+  // 1. Prompt the user for the password criteria
+
+  //    a. Password length 8 < 128
+  const passwordLength = parseInt(prompt('How long will your password be? Between 8-128 characters long'))
+
+  //    b. Lowercase, uppercase, numbers, special characters
+  const uppercaseListed = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const lowercaseListed = 'abcdefghijklmnopqrstuvwxyz'
+  const numberListed = '0123456789'
+  const specialListed = '[]/^_`:;<?@=>!#$%&,()+*-./'
+
+  var listed = uppercaseListed; lowercaseListed; numberListed; specialListed
+  // 2. Validate the input
+
+  // 3. Generate password based on criteria
+  function listed(numberListed,specialListed,lowercaseListed,uppercaseListed) {
+    let charactersList = uppercaseListed
+  }
+  // 4. Display password to the page
+
+}
 
 // Write password to the #password input
 function writePassword() {
