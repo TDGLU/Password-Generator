3a440982ba8619bec8cab80c7d1e6a7da62334c2
TDGLU <tdglukealt@gmail.com>
2022-10-03 14:13:30 -0700

    fixed errors and added more function

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
 // When clicked, this button should display generated password
 generateBtn.addEventListener("click", writePassword)
 
@@ -11,62 +18,80 @@ function generatePassword() {
   // Constants
   const randomFunc = {
     lower: lowerList,
-    upper: UpperList,
+    upper: upperList,
     number: numbList,
-    special: SpecialList
+    special: specialList
   };
-  
-  
-  generateBtn.addEventListener('click', () => {
-    const length = passwordLength.value;
 
-    // Console testing
-    console.log(typeof length)
-  })
-
-// All the valid characters
+  // All the valid characters
   // Lowercase Alphabet from a-z
-function lowerList() {
- return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-}
-
-// Uppercase Alphabet from A-Z
-function UpperList() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-}
-
-// Numbers from 1 - 10
-function numbList() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
-}
-
-// Special characters (14)
-function SpecialList() {
-  return String.fromCharCode(Math.floor(Math.random() * 14) + 33)
-}
-
-//Console testing
-console.log(lowerList(), UpperList(), numbList(), SpecialList());
-
-  // Prompt for the user to input the number of characters the password should have
-  const passwordLength = (prompt('How long will your password be? Choose between 8-128 characters long.'))
-  if (isNaN(passwordLength)) {
-    alert('You must put in a number')
+  function lowerList() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
   }
 
+  // Uppercase Alphabet from A-Z
+  function upperList() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+  }
 
-  // Prompt the user for the password criteria
-  let passLower = confirm('Should your password contain any lowercase letters? a-z')
-  let passUpper = confirm('Should your password contain any Uppercase letters? A-Z')
-  let passNumber = confirm('Should your password contain any numbers? 0-9')
-  let passSpecial = confirm('Should your password contain any special characters?')
-}
+  // Numbers from 1 - 10
+  function numbList() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Special characters (14)
+  function specialList() {
+    return String.fromCharCode(Math.floor(Math.random() * 14) + 33)
+  }
 
-  passwordText.value = password;
+  // Character Length
+  let characters = ''
+  let newPassword = ''
 
+  //Console testing
+  console.log('Randomly generated characters:', lowerList(), upperList(), numbList(), specialList(),'.');
+  
+  // Prompt for the user to input the number of characters the password should have
+  const passwordLength = parseInt(prompt('How long will your password be? Choose between 8-128 characters long.'))
+  
+ // Invalid number message
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    //Console testing
+    console.log('Valid password')
+  }
+    else {
+      alert('Password must be 8 - 128 characters long.')
+      console.log('Invalid password')
+      return 'Invalid input';
+    }
+
+   // Prompt the user for the password criteria
+   let passLower = confirm('Should your password contain any lowercase letters? a-z')
+   let passUpper = confirm('Should your password contain any Uppercase letters? A-Z')
+   let passNumber = confirm('Should your password contain any numbers? 0-9')
+   let passSpecial = confirm('Should your password contain any special characters?')
+
+    if (passLower) {
+      characters += lowerList
+   }
+
+   if (passUpper) {
+     characters += upperList
+   }
+
+   if (passNumber) {
+      characters += numbList
+    }
+
+    if (passSpecial) {
+      characters += specialList
+    }
+   if (!passLower && !passUpper && !passNumber && !passSpecial) {
+      alert('At least pick one lol')
+   }
+
+   for (i = 0; i < passwordLength; i++) {
+     let randomNumber = Math.floor(Math.random() * characters.length)
+     newPassword += characters.charAt(randomNumber)
+  }
 }
